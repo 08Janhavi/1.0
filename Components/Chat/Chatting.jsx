9b9b78dc045db74a21f83d5ebb1e81cc3dfead2d
08Janhavi <08janhavisingh@gmail.com>
2024-01-02 22:58:02 +0530

    Initial commit

---
+++ Components/Chat/Chatting.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { SideBar,Chat,Help,History,Subscription,Sitting} from './index'
+
+const Chatting = () => {
+  return (
+    <section className='chatting-wrapper pt-0'>
+      <SideBar/>
+      <div className="tab-content">
+        <Chat/>
+        <Help/>
+        <History/>
+        <Subscription/>
+        <Sitting/>
+      </div>
+    </section>
+  )
+}
+
+export default Chatting
